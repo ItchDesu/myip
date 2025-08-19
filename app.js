8efee80bed7867f6841a5766a20b52e54d031494
Tu Nombre <you@example.com>
2025-08-20 00:27:55 +0200

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.set('trust proxy', true);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+const SECRET = "0x4AAAAAABs22k14XcjDyB0xrkvKpfZG8ec";
 
 function getClientIPs(req) {
   const xForwardedFor = req.headers['x-forwarded-for'];
@@ -38,6 +45,42 @@ app.get('/', (req, res) => {
 app.get('/api/ip', (req, res) => {
   const { ipv4, ipv6 } = getClientIPs(req);
   res.json({ ipv4, ipv6 });
+});
+
+// Genera token captcha
+// Genera token captcha con delay aleatorio 5-10s
+app.post("/captcha/generate", (req, res) => {
+  const payload = {
+    ip: req.ip,
+    ua: req.headers["user-agent"],
+    ts: Date.now(),
+  };
+
+  const token = jwt.sign(payload, SECRET, { expiresIn: "60s" }); // válido 60 seg
+
+  const delay = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000;
+
+  setTimeout(() => {
+    let score = 0.9;
+    if (!req.headers["user-agent"] || !req.headers["user-agent"].includes("Mozilla")) {
+    }
+
+    res.json({ token, score });
+  }, delay);
+});
+
+// Verificación
+app.post("/verify", (req, res) => {
+  const token = req.body.captcha_token;
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    // Aquí podrías meter lógica de "score" (simple heurística, UA raro, ip sospechosa...)
+    res.cookie("antiddos_ok", "1", { httpOnly: true, secure: false, maxAge: 3600 * 1000 });
+    return res.redirect("/");
+  } catch (err) {
+    return res.status(403).send("❌ Captcha inválido o expirado.");
+  }
 });
 
 app.listen(PORT, () => {
